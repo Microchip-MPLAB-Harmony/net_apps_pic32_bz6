0919b146e214382e9629235f90a7020ce95278c2
Adrian Aur <Adrian.Aur@microchip.com>
2025-04-22 16:55:28 -0700

    [MH3-112203] - Test and regenerate udp_client demo

--- apps/tcpip_udp_client/src/config/pic32wm_bz6_curiosity/initialization.c
+++ apps/tcpip_udp_client/src/config/pic32wm_bz6_curiosity/initialization.c
@@ -551,9 +551,9 @@ void SYS_Initialize ( void* data )
 
     TC0_TimerInitialize();
 
-    SERCOM0_USART_Initialize();
-
     EVSYS_Initialize();
+
+    SERCOM0_USART_Initialize();
 
 	BSP_Initialize();
 
